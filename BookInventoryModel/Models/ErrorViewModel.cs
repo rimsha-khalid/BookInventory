94ba0303651c0720225e6fc5542a70264430edc5
rimsha-khalid <rimsha.khalid@systemsltd.com>
2024-11-13 15:40:34 +0500

    First Commit

---
+++ BookInventoryModel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookInventory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
